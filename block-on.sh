286d62f04cfc4ce630887c38202abe594f995a63
Aanjhan Ranganathan <aanjhan@gmail.com>
2011-02-07 18:44:14 +0100

    added sites into an array

--- block-on.sh
+++ block-on.sh
@@ -3,12 +3,25 @@
 NUMOFARGS=1
 STARTTIME=`date +%R`
 STOPTIME=`date -d "$1 minutes" +%R`
+blacklist=(www.facebook.com twitter.com linkedin.com)
 
+# Some input sanitisation
 if [ $# -ne $NUMOFARGS ]; then
-        echo "Usage : Missing time argument"
+        echo "Usage : Specify the time duration of the block in minutes"
         exit
 fi
 
-
+# Flush iptables
+# This works for me since I dont have any iptables rules usually
 sudo iptables -F
-echo `sudo iptables -A INPUT -p tcp -m time -s www.facebook.com --timestart $STARTTIME --timestop $STOPTIME -j DROP`
+
+# Block the blacklisted sites for the prescribed period
+
+for (( i=0;i<${#blacklist[@]};i++)); do
+    echo "Blocking ${blacklist[${i}]}..."
+    echo `sudo iptables -A OUTPUT -p tcp -m time -d ${blacklist[${i}]} --timestart $STARTTIME --timestop $STOPTIME -j DROP`    
+done
+
+
+
+
