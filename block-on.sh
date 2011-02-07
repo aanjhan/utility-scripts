1c551c6d1bc3dbe234e6d243aa90dc94d44ec1db
Aanjhan Ranganathan <aanjhan@gmail.com>
2011-02-07 18:07:12 +0100

    Some input sanitisation ;-)

--- block-on.sh
+++ block-on.sh
@@ -1,4 +1,14 @@
+#! /bin/bash
+
+NUMOFARGS=1
 STARTTIME=`date +%R`
 STOPTIME=`date -d "$1 minutes" +%R`
+
+if [ $# -ne $NUMOFARGS ]; then
+        echo "Usage : Missing time argument"
+        exit
+fi
+
+
 sudo iptables -F
 echo `sudo iptables -A INPUT -p tcp -m time -s www.facebook.com --timestart $STARTTIME --timestop $STOPTIME -j DROP`
