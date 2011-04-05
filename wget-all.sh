25ce884d28214c9b3a3318c43a619e9caf8fb92f
Aanjhan Ranganathan <aanjhan@gmail.com>
2011-04-05 14:52:15 +0200

    updated to add doctype as well

--- wget-all.sh
+++ wget-all.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 url=$1 # url from to download the files
 docname=$2 # name of the pdfs that repeat without the number
-pdfcount=$3 # number of pdfs/chapters
+doctype=$3 # type of document
+pdfcount=$4 # number of pdfs/chapters
 counter=1
 while [[ $counter -le $pdfcount ]]; do
-	wget "$url/$docname$counter.PDF"
+	wget "$url/$docname$counter.$doctype"
 	counter=$(( $counter + 1 ))
 done
