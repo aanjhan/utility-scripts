949df1dd826ced4ac2a09c9b3fd28d502b55ad25
Aanjhan Ranganathan <aanjhan@gmail.com>
2011-04-05 14:58:03 +0200

    added usage help

--- wget-all.sh
+++ wget-all.sh
@@ -3,6 +3,19 @@ url=$1 # url from to download the files
 docname=$2 # name of the pdfs that repeat without the number
 doctype=$3 # type of document
 pdfcount=$4 # number of pdfs/chapters
+
+# Usage printout
+if [[ $# -ne 4 ]]; then
+	echo "Usage"
+	echo "====="
+	echo "wget-all <url> <docname> <doctype> <doccount>"
+	echo "url - the url of the page from where the docs are to be downloaded"
+	echo "docname - the name of the doc without the chapter/sequence number"
+	echo "doctype - the extension. (case sensitive)"
+	echo "pdfcount - number of documents or end of sequence number"	
+fi
+
+
 counter=1
 while [[ $counter -le $pdfcount ]]; do
 	wget "$url/$docname$counter.$doctype"
